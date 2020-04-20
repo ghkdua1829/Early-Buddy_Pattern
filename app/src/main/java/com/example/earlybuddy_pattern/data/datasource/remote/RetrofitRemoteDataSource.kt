97bb20174d83aeb668f6b3ba3f3814c589d9b6c9
KCY <ghkdua059@gmail.com>
2020-04-20 21:27:39 +0900

    Model 분리

--- app/src/main/java/com/example/earlybuddy_pattern/data/datasource/remote/RetrofitRemoteDataSource.kt
+++ app/src/main/java/com/example/earlybuddy_pattern/data/datasource/remote/RetrofitRemoteDataSource.kt
@@ -1,14 +1,19 @@
 package com.example.earlybuddy_pattern.data.datasource.remote
 
+import com.example.earlybuddy_pattern.data.model.PlaceResponse
 import com.example.earlybuddy_pattern.data.model.UserResponse
 import com.google.gson.JsonObject
+import io.reactivex.Observable
 import retrofit2.Response
 
 interface RetrofitRemoteDataSource {
-    fun signUp(
-        body: JsonObject,
-        onResponse: (Response<UserResponse>) -> Unit,
-        onFailure: (Throwable) -> Unit
-    )
+//    fun signUp(
+//        body: JsonObject,
+//        onResponse: (Response<UserResponse>) -> Unit,
+//        onFailure: (Throwable) -> Unit
+//    )
+    fun singUp(body: JsonObject):Observable<UserResponse>
+
+    fun getAddress(search:String):Observable<PlaceResponse>
 
 }
--- app/src/main/java/com/example/earlybuddy_pattern/data/datasource/remote/RetrofitRemoteDataSourceImpl.kt
+++ app/src/main/java/com/example/earlybuddy_pattern/data/datasource/remote/RetrofitRemoteDataSourceImpl.kt
@@ -1,29 +1,42 @@
 package com.example.earlybuddy_pattern.data.datasource.remote
 
+import android.util.Log
 import com.example.earlybuddy_pattern.EarlyBuddyServiceImpl
+import com.example.earlybuddy_pattern.data.model.PlaceResponse
 import com.example.earlybuddy_pattern.data.model.UserResponse
 import com.google.gson.JsonObject
-import retrofit2.Call
-import retrofit2.Response
+import io.reactivex.Observable
 
 class RetrofitRemoteDataSourceImpl : RetrofitRemoteDataSource {
-    override fun signUp(
-        body: JsonObject,
-        onResponse: (Response<UserResponse>) -> Unit,
-        onFailure: (Throwable) -> Unit
-    ) {
-        EarlyBuddyServiceImpl.service.postSignupUser(body)
-            .enqueue(object : retrofit2.Callback<UserResponse> {
-                override fun onFailure(call: Call<UserResponse>, t: Throwable) {
-                    onFailure(t)
-                }
+    val api = EarlyBuddyServiceImpl.service
+    override fun getAddress(search: String): Observable<PlaceResponse> =
+        api.getAdress(search)
+            .doOnError {
+                Log.e("gu, ':!!!!!", "s")
+                it.printStackTrace() }
+            .map { it }
 
-                override fun onResponse(
-                    call: Call<UserResponse>,
-                    response: Response<UserResponse>
-                ) {
-                    onResponse(response)
-                }
-            })
+    override fun singUp(body: JsonObject): Observable<UserResponse> {
+        return api.postSignupUser(body).map { it }
     }
+
+//    override fun signUp(
+//        body: JsonObject,
+//        onResponse: (Response<UserResponse>) -> Unit,
+//        onFailure: (Throwable) -> Unit
+//    ) {
+//        EarlyBuddyServiceImpl.service.postSignupUser(body)
+//            .enqueue(object : retrofit2.Callback<UserResponse> {
+//                override fun onFailure(call: Call<UserResponse>, t: Throwable) {
+//                    onFailure(t)
+//                }
+//
+//                override fun onResponse(
+//                    call: Call<UserResponse>,
+//                    response: Response<UserResponse>
+//                ) {
+//                    onResponse(response)
+//                }
+//            })
+//    }
 }
