ab45fb9c2fa8bc1e7d9c453b0b54bedd80de5587
KCY <ghkdua059@gmail.com>
2020-04-20 21:28:04 +0900

    Repository 생성

--- app/src/main/java/com/example/earlybuddy_pattern/data/repository/AddressRepository.kt
+++ app/src/main/java/com/example/earlybuddy_pattern/data/repository/AddressRepository.kt
@@ -1,4 +1,10 @@
 package com.example.earlybuddy_pattern.data.repository
 
+import com.example.earlybuddy_pattern.data.datasource.remote.RetrofitRemoteDataSource
+import com.example.earlybuddy_pattern.data.datasource.remote.RetrofitRemoteDataSourceImpl
+
 class AddressRepository {
+    val RetrofitRemoteDataSource: RetrofitRemoteDataSource = RetrofitRemoteDataSourceImpl()
+
+    fun getAddress(serach: String) = RetrofitRemoteDataSource.getAddress(serach)
 }
--- app/src/main/java/com/example/earlybuddy_pattern/data/repository/SignUpRepository.kt
+++ app/src/main/java/com/example/earlybuddy_pattern/data/repository/SignUpRepository.kt
@@ -2,17 +2,17 @@ package com.example.earlybuddy_pattern.data.repository
 
 import com.example.earlybuddy_pattern.data.datasource.remote.RetrofitRemoteDataSource
 import com.example.earlybuddy_pattern.data.datasource.remote.RetrofitRemoteDataSourceImpl
-import com.example.earlybuddy_pattern.data.model.UserResponse
 import com.google.gson.JsonObject
-import retrofit2.Response
 
 class SignUpRepository {
     val RetrofitRemoteDataSource: RetrofitRemoteDataSource = RetrofitRemoteDataSourceImpl()
-    fun signUp(
-        body: JsonObject,
-        success: (Response<UserResponse>) -> Unit,
-        fail: (Throwable) -> Unit
-    ) {
-        RetrofitRemoteDataSource.signUp(body, success, fail)
-    }
+//    fun signUp(
+//        body: JsonObject,
+//        success: (Response<UserResponse>) -> Unit,
+//        fail: (Throwable) -> Unit
+//    ) {
+//        RetrofitRemoteDataSource.signUp(body, success, fail)
+//    }
+
+    fun signUp(body: JsonObject) = RetrofitRemoteDataSource.singUp(body)
 }
