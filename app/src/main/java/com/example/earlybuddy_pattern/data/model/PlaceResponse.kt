b7f078cae0e20a24dd36d4edc3d4a1cf583f73dd
KCY <ghkdua059@gmail.com>
2020-04-20 21:27:53 +0900

    데이터 클래스 생성

--- app/src/main/java/com/example/earlybuddy_pattern/data/model/PlaceResponse.kt
+++ app/src/main/java/com/example/earlybuddy_pattern/data/model/PlaceResponse.kt
@@ -1,2 +1,15 @@
 package com.example.earlybuddy_pattern.data.model
 
+data class PlaceResponse(
+    val status: Boolean,
+    val message: List<Place>
+
+)
+
+data class Place(
+    val placeName :String,
+    val addressName: String,
+    val roadAddressName: String,
+    val x: String,
+    val y: String
+)
