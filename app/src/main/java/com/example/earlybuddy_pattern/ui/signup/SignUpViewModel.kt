52454415cda350f1031e899cbad3f9c8623c99a4
KCY <ghkdua059@gmail.com>
2020-04-20 21:28:56 +0900

    뷰모델 생성

--- app/src/main/java/com/example/earlybuddy_pattern/ui/signup/SignUpViewModel.kt
+++ app/src/main/java/com/example/earlybuddy_pattern/ui/signup/SignUpViewModel.kt
@@ -1,2 +1,45 @@
 package com.example.earlybuddy_pattern.ui.signup
 
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.earlybuddy_pattern.data.repository.AddressRepository
+import com.example.earlybuddy_pattern.data.repository.SignUpRepository
+import com.google.gson.JsonObject
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+
+class SignUpViewModel : ViewModel() {
+    private val SignUpRepository: SignUpRepository = SignUpRepository()
+    val isSuccessNetwork = MutableLiveData<Boolean>()
+    val turnRed = MutableLiveData<Boolean>()
+    val successUserNum = MutableLiveData<Int>()
+    val wifiDisconnect = MutableLiveData<Unit>()
+    private val disposables = CompositeDisposable()
+
+    fun signUp(body: JsonObject) {
+
+        disposables.add(SignUpRepository.signUp(body)
+            .observeOn(AndroidSchedulers.mainThread())
+            .doOnSubscribe{}
+            .doOnTerminate{}
+            .subscribe({
+                successUserNum.value = it.data
+            }){
+                wifiDisconnect.value = Unit
+            })
+
+
+//        SignUpRepository.signUp(body = body, success = {
+//            isSuccessNetwork.value = it.isSuccessful
+//            if(isSuccessNetwork.value == true){
+//                successUserNum.value = it.body()!!.data
+//            }else{
+//                turnRed.value =true
+//            }
+//        }, fail = {
+//            wifiDisconnect.value = Unit
+//        }
+//        )
+    }
+
+}
--- app/src/main/java/com/example/earlybuddy_pattern/ui/visited/VisitedPlaceViewModel.kt
+++ app/src/main/java/com/example/earlybuddy_pattern/ui/visited/VisitedPlaceViewModel.kt
@@ -1,2 +1,26 @@
 package com.example.earlybuddy_pattern.ui.visited
 
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.earlybuddy_pattern.data.model.Place
+import com.example.earlybuddy_pattern.data.repository.AddressRepository
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+
+class VisitedPlaceViewModel : ViewModel() {
+    private val addressRepository: AddressRepository = AddressRepository()
+    private val disposables = CompositeDisposable()
+    val PlaceList = MutableLiveData<List<Place>>()
+
+    fun getAddress(search: String) {
+        disposables.add(addressRepository.getAddress(search)
+            .observeOn(AndroidSchedulers.mainThread())
+            .doOnSubscribe {
+            }
+            .doOnTerminate {}
+            .subscribe {
+                PlaceList.value = it.message
+            }
+        )
+    }
+}
