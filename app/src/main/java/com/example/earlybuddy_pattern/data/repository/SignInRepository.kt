41366aad369f4d06a7b38428623861efb47f1aea
KCY <ghkdua059@gmail.com>
2020-04-21 22:19:21 +0900

    로그인 레포지토리 추가

--- app/src/main/java/com/example/earlybuddy_pattern/data/repository/SignInRepository.kt
+++ app/src/main/java/com/example/earlybuddy_pattern/data/repository/SignInRepository.kt
@@ -1,2 +1,11 @@
 package com.example.earlybuddy_pattern.data.repository
 
+import com.example.earlybuddy_pattern.data.datasource.remote.RetrofitRemoteDataSourceImpl
+import com.google.gson.JsonObject
+
+class SignInRepository {
+
+    val RetrofitRemoteDataSource = RetrofitRemoteDataSourceImpl()
+
+    fun signIn(body: JsonObject) = RetrofitRemoteDataSource.signIn(body)
+}
