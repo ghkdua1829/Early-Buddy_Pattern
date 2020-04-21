9f3eba7a1fd0b36b12baf80182b1e20f47e86a2b
KCY <ghkdua059@gmail.com>
2020-04-21 22:20:20 +0900

    로그인 뷰모델 구현

--- app/src/main/java/com/example/earlybuddy_pattern/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/example/earlybuddy_pattern/ui/login/LoginViewModel.kt
@@ -1,2 +1,31 @@
 package com.example.earlybuddy_pattern.ui.login
 
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.earlybuddy_pattern.data.model.SignInUser
+import com.example.earlybuddy_pattern.data.repository.SignInRepository
+import com.google.gson.JsonObject
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+
+class LoginViewModel : ViewModel() {
+    val signinRepository = SignInRepository()
+
+    val failedSignIn = MutableLiveData<Throwable>()
+    val successedSignIn = MutableLiveData<SignInUser>()
+
+    private val disposables = CompositeDisposable()
+
+    fun signIn(body: JsonObject) {
+        disposables.add(signinRepository.signIn(body)
+            .observeOn(AndroidSchedulers.mainThread())
+            .doOnSubscribe {}
+            .doOnTerminate {}
+            .subscribe({
+                successedSignIn.value = it.data
+            }) {
+                failedSignIn.value = it
+            })
+    }
+
+}
